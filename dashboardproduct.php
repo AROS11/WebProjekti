e225e34bd529dc73efa77b005ede9ac02842d69a
Artan Osmani <ao56171@ubt-uni.net>
2023-02-20 22:43:12 +0100

    Ndryshime ne css

--- dashboardproduct.php
+++ dashboardproduct.php
@@ -6,40 +6,7 @@ $products = $mapper->getAllProducts();
 if(isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] == 1) {
 ?> 
 
-<style>
-.produktet{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 550px;
-}
-.tablla{
-  width: 50%;
-  border:1px rgba(128, 128, 128, 0.5) solid;
-  background-color: rgba(128, 128, 128, 0.3);
-}
-.tablla th {
-  text-align: center;
-  height: 30px;
-  width: 20%;
-  border: 2px rgba(128, 128, 128, 0.5) solid;
-}
-.tablla td {
-  text-align: center;
-  height: 30px;
-  width: 20%;
-  border: 2px rgba(128, 128, 128, 0.5) solid;
- text-decoration: none;
-}
-.tablla td a{
- text-decoration: none;
- font-family: Arial, Helvetica, sans-serif;
- color: red;
- font-weight:bold;
-}
 
-</style>
     <div class="produktet">
             <h3>Produktet tona</h3>
             <table class="tablla">
